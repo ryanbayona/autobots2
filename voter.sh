de907a71ec49f17bea7bb9147b3c499c016b99a0
Ryan Bayona <ryanbayona@gmai.com>
2026-07-21 07:49:27 +0800

    new approach - embedded

--- voter.sh
+++ voter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 count=0
-LOG="my.log"
+
 
 while true; do
     start=$(date '+%Y-%m-%d %H:%M:%S')
@@ -9,12 +9,8 @@ while true; do
     echo "==================================================" 
     echo "START: $start | Iteration: $((count + 1))" 
     if npx playwright test \
-        tests/ppop-poll.spec.ts \
+        tests/embed.spec.ts \
         --project=chromium \
-        --project=firefox \
-        --project=webkit \
-        --project=mobile-safari \
-        --project=ms-edge \
         --reporter=line 
     then
         status=0
@@ -27,14 +23,14 @@ while true; do
     echo "END:   $end | Iteration: $count | Exit: $status" 
 
     echo "Iteration: $count (exit=$status)"
-    sleep 5
+    sleep 0.5
     #if (( count >= 12 )) || (( status != 0 )); then
-    if (( count >= 12 )); then
-        echo "Sleeping 60 seconds..."
-        sleep 60
-        count=0      # Optional: restart the iteration count
-    else
-        sleep 5
-    fi
+    #if (( count >= 12 )); then
+    #    echo "Sleeping 60 seconds..."
+    #    sleep 60
+    #    count=0      # Optional: restart the iteration count
+    #else
+    #    sleep 5
+    #fi
 done
 
